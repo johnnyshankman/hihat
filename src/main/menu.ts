96c9d05cad516361bf93d28a1805d508711b2692
Johnny Shankman`` <johnnyshankman@users.noreply.github.com>
2023-11-04 22:42:16 -0400

    remove unused menu items

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -76,7 +76,7 @@ export default class MenuBuilder {
         { label: 'show all', selector: 'unhideAllApplications:' },
         { type: 'separator' },
         {
-          label: 'quit!',
+          label: 'close hihat',
           accelerator: 'Command+Q',
           click: () => {
             app.quit();
@@ -155,29 +155,19 @@ export default class MenuBuilder {
       label: 'Help',
       submenu: [
         {
-          label: 'Learn More',
-          click() {
-            shell.openExternal('https://electronjs.org');
-          },
-        },
-        {
           label: 'Documentation',
           click() {
             shell.openExternal(
-              'https://github.com/electron/electron/tree/main/docs#readme',
+              'https://github.com/johnnyshankman/hihat/tree/main/docs#readme',
             );
-          },
-        },
-        {
-          label: 'Community Discussions',
-          click() {
-            shell.openExternal('https://www.electronjs.org/community');
           },
         },
         {
           label: 'Search Issues',
           click() {
-            shell.openExternal('https://github.com/electron/electron/issues');
+            shell.openExternal(
+              'https://github.com/johnnyshankman/hihat/issues',
+            );
           },
         },
       ],
@@ -194,22 +184,6 @@ export default class MenuBuilder {
 
   buildDefaultTemplate() {
     const templateDefault = [
-      {
-        label: '&File',
-        submenu: [
-          {
-            label: '&Open',
-            accelerator: 'Ctrl+O',
-          },
-          {
-            label: '&Close',
-            accelerator: 'Ctrl+W',
-            click: () => {
-              this.mainWindow.close();
-            },
-          },
-        ],
-      },
       {
         label: '&View',
         submenu:
@@ -256,29 +230,19 @@ export default class MenuBuilder {
         label: 'Help',
         submenu: [
           {
-            label: 'Learn More',
-            click() {
-              shell.openExternal('https://electronjs.org');
-            },
-          },
-          {
             label: 'Documentation',
             click() {
               shell.openExternal(
-                'https://github.com/electron/electron/tree/main/docs#readme',
+                'https://github.com/johnnyshankman/hihat/tree/main/docs#readme',
               );
-            },
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://www.electronjs.org/community');
             },
           },
           {
             label: 'Search Issues',
             click() {
-              shell.openExternal('https://github.com/electron/electron/issues');
+              shell.openExternal(
+                'https://github.com/johnnyshankman/hihat/issues',
+              );
             },
           },
         ],
