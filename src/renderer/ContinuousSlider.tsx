15308e1ab886a712b0b4ab5f9788f1bdda4041a1
Johnny Shankman`` <johnnyshankman@users.noreply.github.com>
2023-12-15 12:05:08 -0500

    add repeat feature

--- src/renderer/ContinuousSlider.tsx
+++ src/renderer/ContinuousSlider.tsx
@@ -7,16 +7,21 @@ import VolumeUp from '@mui/icons-material/VolumeUp';
 import { IconButton } from '@mui/material';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 import ShuffleOnIcon from '@mui/icons-material/ShuffleOn';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import RepeatOnIcon from '@mui/icons-material/RepeatOn';
 
 export default function ContinuousSlider({
   onChange,
   onShuffleChange,
+  onRepeatingChange,
 }: {
   onChange: (event: Event, newValue: number | number[]) => void;
   onShuffleChange: (isShuffled: boolean) => void;
+  onRepeatingChange: (isRepeating: boolean) => void;
 }) {
   const [value, setValue] = React.useState<number>(100);
   const [isShuffled, setIsShuffled] = React.useState<boolean>(false);
+  const [isRepeating, setIsRepeating] = React.useState<boolean>(false);
 
   const handleChange = (event: Event, newValue: number | number[]) => {
     setValue(newValue as number);
@@ -30,28 +35,62 @@ export default function ContinuousSlider({
         direction="row"
         alignItems="center"
         justifyContent="center"
-        justifyItems="center"
+        justifyItems="start"
       >
-        <IconButton
-          sx={{
-            fontSize: '1rem',
-            // only on small sized screens make it absolutely positioned
-            position: { xs: 'absolute', sm: 'relative' },
-            left: { xs: 0, sm: 'auto' },
-            color: 'rgb(133,133,133)',
-          }}
-          onClick={() => {
-            const newValue = !isShuffled;
-            setIsShuffled(newValue);
-            onShuffleChange(newValue);
-          }}
+        <Stack
+          spacing={0}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          justifyItems="center"
         >
-          {isShuffled ? (
-            <ShuffleOnIcon fontSize="inherit" />
-          ) : (
-            <ShuffleIcon fontSize="inherit" />
-          )}
-        </IconButton>
+          <IconButton
+            sx={{
+              fontSize: '1rem',
+
+              position: {
+                xs: 'absolute',
+                sm: 'relative',
+              },
+              left: { xs: 0, sm: 'auto' },
+              color: 'rgb(133,133,133)',
+            }}
+            onClick={() => {
+              const newValue = !isRepeating;
+              setIsRepeating(newValue);
+              onRepeatingChange(newValue);
+            }}
+          >
+            {isRepeating ? (
+              <RepeatOnIcon fontSize="inherit" />
+            ) : (
+              <RepeatIcon fontSize="inherit" />
+            )}
+          </IconButton>
+          <IconButton
+            sx={{
+              fontSize: '1rem',
+              position: {
+                // @dev on small sized screens make it absolute
+                xs: 'absolute',
+                sm: 'relative',
+              },
+              left: { xs: '1.5rem', sm: 'auto' },
+              color: 'rgb(133,133,133)',
+            }}
+            onClick={() => {
+              const newValue = !isShuffled;
+              setIsShuffled(newValue);
+              onShuffleChange(newValue);
+            }}
+          >
+            {isShuffled ? (
+              <ShuffleOnIcon fontSize="inherit" />
+            ) : (
+              <ShuffleIcon fontSize="inherit" />
+            )}
+          </IconButton>
+        </Stack>
         <VolumeDown fontSize="small" />
         <Slider
           size="small"
